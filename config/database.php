8f82045ba3082e40791949842ca5fa38f4216a17
v_xingwanying <v_xingwanying@baidu.com>
2016-11-23 11:21:05 +0800

    修改数据库配置

--- config/database.php
+++ config/database.php
@@ -78,6 +78,16 @@ return [
             'schema' => 'public',
             'sslmode' => 'prefer',
         ],
+        'heroku' => [
+            'driver'   => 'pgsql',
+            'host'     => parse_url(getenv("DATABASE_URL"))["host"],
+            'database' => substr(parse_url(getenv("DATABASE_URL"))["path"], 1),
+            'username' => parse_url(getenv("DATABASE_URL"))["user"],
+            'password' => parse_url(getenv("DATABASE_URL"))["pass"],
+            'charset'  => 'utf8',
+            'prefix'   => '',
+            'schema'   => 'public',
+        ],
 
     ],
 
